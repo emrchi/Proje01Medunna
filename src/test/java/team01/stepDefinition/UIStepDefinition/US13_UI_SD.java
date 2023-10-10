2347ed4e9ed63c38a73c074c1606e438be462cba
Yasemin Eric <yasemineric6@gmail.com>
2023-10-10 22:45:38 +0200

    10.10.US12

--- src/test/java/team01/stepDefinition/UIStepDefinition/US13_UI_SD.java
+++ src/test/java/team01/stepDefinition/UIStepDefinition/US13_UI_SD.java
@@ -1,4 +1,35 @@
 package team01.stepDefinition.UIStepDefinition;
 
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import team01.pages.DoctorPages;
+import team01.utilities.Driver;
+import team01.utilities.JSUtils;
+import team01.utilities.ReusableMethods;
+
 public class US13_UI_SD {
+    DoctorPages doctorPages=new DoctorPages();
+
+    @And("Kullanici Show Test Results butonuna tiklar")
+    public void kullaniciShowTestResultsButonunaTiklar() {
+        ReusableMethods.bekle(2);
+        JSUtils.scrollIntoViewJS(doctorPages.showTestResultsY);
+        JSUtils.clickElementByJS(doctorPages.showTestResultsY);
+    }
+
+    @And("Kullanici testin yanindaki view Results butonuna tiklar")
+    public void kullaniciTestinYanindakiViewResultsButonunaTiklar(DataTable data) {
+        WebElement viewResultsY = Driver.getDriver().findElement(By.xpath("//td[a[text()='"+data.row(1).get(0)+"']]/../td[7]//span"));
+        JSUtils.clickElementByJS(viewResultsY);
+    }
+
+
+    @And("Kullanici gerekli verilerin göründügünü dogrular; id {string},name {string}, result {string}, default max value {string},default min value {string},test {string}, description {string},the date {string}")
+    public void kullaniciGerekliVerilerinGöründügünüDogrularIdNameResultDefaultMaxValueDefaultMinValueTestDescriptionTheDate(String id, String name, String result, String defaultMaxValue, String defaultMinValue, String test, String description, String theDate) {
+
+
+
+    }
 }
--- src/test/resources/features/UI/US_12_UI.feature
+++ src/test/resources/features/UI/US_12_UI.feature
@@ -1,3 +1,40 @@
-Feature: US01
-  Scenario: TC01
-    Given "Admin" hasabiyla logil ol
+@US_12
+Feature: US12
+  Background:
+    Given Doktor "medunna" sayfasina gider
+    When "Doctor" hesabiyla login ol
+    Then Doktor My Pages sekmesine tiklar
+    Then Doktor cikan menüde My Appointments sekmesine tiklar
+
+  @US12_TC01
+  Scenario: TC01_Physician (Doktor) test isteyebilmeli
+    And Doktor sayfada hastanin yanindaki edit butonuna tiklar
+    |Patient|
+    |mrv    |
+    And Doktor Request a Test butonuna tiklar
+    And Doktor test iceriginde gerekli verilerin görüngügünü dogrular
+    |Testler|
+    |Glucose|
+    |Urea   |
+    |Creatinine|
+    |Sodium    |
+    |Potassium |
+    |Total Protein|
+    |Albumin      |
+    |Hemoglobin   |
+    And Doktor test secer;
+    |Test|
+    |Urea|
+    And Doktor save butonuna tiklar
+    And Doktor test talebinin basarili bir sekilde gerceklestirildigini dogrular
+
+
+
+    @US12_TC02
+    Scenario: TC02_Negatif Senaryo
+      And Doktor sayfada hastanin yanindaki edit butonuna tiklar
+        |Patient|
+        |mrv    |
+      And Doktor Request a Test butonuna tiklar
+      And Doktor save butonuna tiklar
+      And Doktor test talebinin gerceklesmedigini dogrular
