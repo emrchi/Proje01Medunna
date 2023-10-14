394262f437e8fb099bc69d341850f0a495663df5
Hamza Say <hmzsy.27@gmail.com>
2023-10-14 23:41:03 +0300

    US 17-18-19 ui first

--- src/test/java/team01/stepDefinition/UIStepDefinition/US18_UI_SD.java
+++ src/test/java/team01/stepDefinition/UIStepDefinition/US18_UI_SD.java
@@ -1,4 +1,188 @@
 package team01.stepDefinition.UIStepDefinition;
 
+import io.cucumber.java.en.And;
+import team01.pages.AdminPages;
+import team01.utilities.Driver;
+import team01.utilities.ReusableMethods;
+
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
 public class US18_UI_SD {
+    AdminPages adminPages = new AdminPages();
+
+    @And("Clicks on Physician")
+    public void clicksOnPhysician() {
+        ReusableMethods.click(adminPages.physcian);
+    }
+
+    @And("Clicks on +Create a new Physician")
+    public void clicksOnCreateANewPhysician() {
+        ReusableMethods.click(adminPages.createANewItem);
+    }
+
+    @And("Click on the use search box")
+    public void clickOnTheUseSearchBox() {
+        ReusableMethods.click(adminPages.useSearch);
+    }
+
+    @And("Enter a {string} in the SSN field")
+    public void enterAInTheSSNField(String ssnNumber) {
+        adminPages.searchSSN.sendKeys(ssnNumber);
+    }
+
+    @And("Click on the Search User")
+    public void clickOnTheSearchUser() {
+        ReusableMethods.click(adminPages.searchUser);
+    }
+
+    @And("Clear and enter a Birth date {string}")
+    public void clearAndEnterABirthDate(String birthDate) {
+        adminPages.physicianBirthDate.clear();
+        adminPages.physicianBirthDate.sendKeys(birthDate);
+    }
+
+    @And("Enter a phone number {string}")
+    public void enterAPhoneNumber(String phoneNumber) {
+        adminPages.physicianPhoneNumber.sendKeys(phoneNumber);
+    }
+
+    @And("Enter a Address information {string}")
+    public void enterAAddressInformation(String adress) {
+        adminPages.physicianAdress.sendKeys(adress);
+    }
+
+    @And("Select Gender")
+    public void selectGender() {
+        ReusableMethods.ddmValue(adminPages.physicianGender, "MALE");
+    }
+
+    @And("Select Speciality")
+    public void selectSpeciality() {
+        ReusableMethods.ddmValue(adminPages.physicianSpeciality, "PSYCHIATRY");
+    }
+
+    @And("Select Blood Group")
+    public void selectBloodGroup() {
+        ReusableMethods.ddmIndex(adminPages.physicianBloodGroup, 4);
+    }
+
+    @And("Enter a description {string}")
+    public void enterADescription(String description) {
+        adminPages.physicianDescription.sendKeys(description);
+    }
+
+
+    @And("Upload the image")
+    public void uploadTheImage() {
+        // ReusableMethods.click(adminPages.physicianChooseFile);
+        String imagePath = "C:\\Users\\90553\\Desktop\\260520231-1080x720.jpg";
+        //ReusableMethods.uploadFile(imagePath);
+        adminPages.physicianChooseFile.sendKeys(imagePath);
+
+
+    }
+
+    @And("Enter a Exam Fee {string}")
+    public void enterAExamFee(String examFee) {
+        adminPages.physicianExamFee.clear();
+        adminPages.physicianExamFee.sendKeys(examFee);
+    }
+
+    @And("Select the Country")
+    public void selectTheCountry() {
+        ReusableMethods.ddmVisibleText(adminPages.physicianCountry,"USA");
+    }
+
+    @And("Select the State or City")
+    public void selectTheStateOrCity() {
+        ReusableMethods.ddmVisibleText(adminPages.physicianState,"NEWYORK");
+    }
+
+    @And("Click on the edit of the ssn number {string}")
+    public void clickOnTheEditOfTheSsnNumber(String ssnNumber) {
+        ReusableMethods.click(adminPages.physicianEdit);
+    }
+
+    @And("Clicks on physcian Created Date")
+    public void clicksOnPhyscianCreatedDate() {
+        ReusableMethods.click(adminPages.physicianCreatedDate);
+        ReusableMethods.bekle(5);
+    }
+
+    @And("Clear and enter a new first name {string}")
+    public void clearAndEnterANewFirstName(String editPhysician) {
+        adminPages.physicianFirstName.clear();
+        adminPages.physicianFirstName.sendKeys(editPhysician);
+    }
+
+    @And("Clear and enter a new last name {string}")
+    public void clearAndEnterANewLastName(String lastName) {
+        adminPages.physicianLastName.clear();
+        adminPages.physicianLastName.sendKeys(lastName);
+    }
+
+    @And("Clear and enter new Birth date {string}")
+    public void clearAndEnterNewBirthDate(String newBirthDate) {
+        adminPages.physicianBirthDate.clear();
+        adminPages.physicianBirthDate.sendKeys(newBirthDate);
+    }
+
+    @And("Clear and enter a new Phone Number {string}")
+    public void clearAndEnterANewPhoneNumber(String newPhoneNumber) {
+        adminPages.physicianPhoneNumber.clear();
+        adminPages.physicianPhoneNumber.sendKeys(newPhoneNumber);
+    }
+
+    @And("Clear and enter a new Address {string}")
+    public void clearAndEnterANewAddress(String newAdress) {
+        adminPages.physicianAdress.clear();
+        adminPages.physicianAdress.sendKeys(newAdress);
+    }
+    @And("Select new Speciality")
+    public void selectNewSpeciality() {
+        ReusableMethods.ddmVisibleText(adminPages.physicianSpeciality,"Family_medicine");
+    }
+
+    @And("Select new Blood Group")
+    public void selectNewBloodGroup() {
+        ReusableMethods.ddmIndex(adminPages.physicianBloodGroup, 5);
+
+    }
+
+    @And("Clear and enter a new Description {string}")
+    public void clearAndEnterANewDescription(String newDescription) {
+        adminPages.physicianDescription.clear();
+        adminPages.physicianDescription.sendKeys(newDescription);
+    }
+
+    @And("Clear and upload a new image")
+    public void clearAndUploadANewImage() {
+        adminPages.physicianChooseFile.clear();
+        String newImagePath="C:\\Users\\90553\\Desktop\\mecnunçınar.jpg";
+        adminPages.physicianChooseFile.sendKeys(newImagePath);
+    }
+
+    @And("Clear and enter a new Exam Fee {string}")
+    public void clearAndEnterANewExamFee(String newExamFee) {
+        adminPages.physicianExamFee.clear();
+        adminPages.physicianExamFee.sendKeys(newExamFee);
+    }
+
+    @And("Select the new Country")
+    public void selectTheNewCountry() {
+        ReusableMethods.ddmVisibleText(adminPages.physicianCountry,"Turkey");
+    }
+
+    @And("Click on the view of the ssn number {string}")
+    public void clickOnTheViewOfTheSsnNumber(String ssnNumber) {
+        ReusableMethods.click(adminPages.physicianView);
+    }
+
+
+    @And("Click on the Delete of the one with the ssn number {string}")
+    public void clickOnTheDeleteOfTheOneWithTheSsnNumber(String ssnNumber) {
+        ReusableMethods.click(adminPages.physicianDelete);
+    }
 }
--- src/test/resources/features/UI/US_17_UI.feature
+++ src/test/resources/features/UI/US_17_UI.feature
@@ -1,3 +1,80 @@
-Feature: US01
-  Scenario: TC01
-    Given "Admin" hasabiyla logil ol
+@ui17-18
+Feature: US 17 Create Update  Delete test items
+
+  Background: Common Process
+    Given User go to homepage "url" with properties
+    When signs in as "Admin"
+    And Clicks on Items&Titles
+    And Clicks on Test Item
+
+  Scenario: TC_001 Create Test Items as Admin
+    And Clicks on +Create a new Test Item
+    And Enter a "Blood Test" in the name field
+    And Enter a "Routine Control" in the description field
+    And Enter a "125" in the price field
+    And Enter a "13.0 g/dL" in default min. value
+    And Enter a "17.0 g/dL" in default max. value
+    And Click on the save button
+    Then Verify the text "A new Test Item is created with identifier"
+    And Close the Driver
+
+  Scenario: TC_002 Update Test Items as Admin
+    And Clicks on Created Date
+    And Click on the edit of the one with the name blood test
+    And Clear and enter a new test name "Hemogram"
+    And Clear and enter a new description "Control"
+    And Clear and enter a new price "150"
+    And Clear and enter a new default min value "13.1 g/dL"
+    And Clear and enter a new default max value "17.2 g/dL"
+    And Click on the save button
+    Then Verify the text "A Test Item is updated with identifier"
+    And Close the Driver
+
+  Scenario: TC_003 Visible and Delete Test Items as Admin
+    And Clicks on Created Date
+    And Click on the view of the one with the name Hemogram
+    Then Verify that the Name contains a "Hemogram"
+    And Verify that the Description contains a "Control"
+    And Verify that the Price contains a "150"
+    And Verify that the Default Min. Value contains a "13.1 g/dL"
+    And Verify that the Default Max. Value contains a "17.2 g/dL"
+    And Verify that the created date is visible
+    When Click on the Back button
+    And Clicks on Created Date
+    And Click on the Delete of the one with the test name Hemogram
+    And Click the delete button on the confirm delete operation alert
+    Then Verify the text "A Test Item is deleted with identifier"
+    And Close the Driver
+
+  Scenario: TC_004 Create Test Items as Admin without entering Description Default Min. Value and Default Max. Value values
+    And Clicks on +Create a new Test Item
+    And Enter a "Hemogram" in the name field
+    And Enter a "125" in the price field
+    And Click on the save button
+    Then Verify the text "A new Test Item is created with identifier"
+    And Close the Driver
+
+
+  Scenario: TC_005 Create Test Items as an Admin by Entering Negative Numbers in the Price section
+    And Clicks on +Create a new Test Item
+    And Enter a "Hemogram" in the name field
+    And Enter a "-125" in the price field
+    And Click on the save button
+    Then Verify the text This field should be at least zero
+    And Close the Driver
+
+  Scenario: TC_006 Test Items not be Created With Empty Data
+    And Clicks on +Create a new Test Item
+    And Click on the save button
+    Then Verify the text This field is required in the Name field
+    And  Verify the text This field is required in the Price field
+
+
+
+
+
+
+
+
+
+
